430ad805f01f4c4eb6d92dc8124e101d4d0f43a6
onlyver <1072539675@qq.com>
2023-11-05 09:23:12 +0800

    init

---
+++ LVSwiftMall/LVSwiftMall/LVSwiftMallApp.swift
@@ -0,0 +1,17 @@
+//
+//  LVSwiftMallApp.swift
+//  LVSwiftMall
+//
+//  Created by LiLver on 2023/11/5.
+//
+
+import SwiftUI
+
+@main
+struct LVSwiftMallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
